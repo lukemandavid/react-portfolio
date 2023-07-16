c7bdd85aafb2aed7b4380ba8e520214d1e4244c5
lukemandavid <dmwangi595@gmail.com>
2023-07-16 18:57:20 +0300

    updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <header>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/">Contact</Link>
+        </nav>
+      </header>
+    </>
+  );
 };
 
 export default Header;
