61f4163bcb2aec7b305d1ba22cda1b0b5daa887d
lukemandavid <dmwangi595@gmail.com>
2023-07-16 19:00:08 +0300

    updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <nav>
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
-          <Link to="/">Contact</Link>
+          <Link to="/contact">Contact</Link>
         </nav>
       </header>
     </>
